e58a9e738f65246392b11f71979acd93093bbf68
김선혜 <kimsunhye1225@gmail.com>
2025-05-25 17:03:04 +0900

    [김선혜] - 로드맵 페이지 사진 넣기 전까지 완료

--- frontend/graduation-project/src/pages/RoadmapPage.js
+++ frontend/graduation-project/src/pages/RoadmapPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../pages-css/JobDetailPage.css';
 import '../pages-css/RoadmapPage.css';
@@ -9,6 +9,7 @@ function RoadmapPage() {
   const jobName = location.state?.jobName || '직업 미지정';
 
   const imageUrl = `${process.env.PUBLIC_URL}/img/roadmap-${jobId}.png`;
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div className="RoadmapPage">
@@ -17,8 +18,24 @@ function RoadmapPage() {
       </div>
 
       <div className="RoadmapCard">
-        <img src={imageUrl} alt="로드맵 이미지" className="roadmap-image" />
+        <img
+          src={imageUrl}
+          alt="로드맵 이미지"
+          className="roadmap-image"
+          onClick={() => setIsModalOpen(true)}
+          style={{ cursor: 'zoom-in' }}
+        />
       </div>
+
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={() => setIsModalOpen(false)}>
+          <img
+            src={imageUrl}
+            alt="확대된 로드맵 이미지"
+            className="modal-image"
+          />
+        </div>
+      )}
     </div>
   );
 }
