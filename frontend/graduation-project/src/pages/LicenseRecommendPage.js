28e24a4531816198f963cf48059e10c53ff62d87
김선혜 <kimsunhye1225@gmail.com>
2025-05-25 15:38:28 +0900

    [김선혜] - 자격증 화면 모달 기능 추가

--- frontend/graduation-project/src/pages/LicenseRecommendPage.js
+++ frontend/graduation-project/src/pages/LicenseRecommendPage.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../pages-css/LicenseRecommendPage.css';
-import '../pages-css/JobDetailPage.css'; // JobDetail-top, SelectedJob 스타일 재사용
+import '../pages-css/JobDetailPage.css';
 
 function LicenseRecommendPage() {
   const location = useLocation();
-
   const jobId = location.state?.jobId || 'JOB001';
   const jobName = location.state?.jobName || '직업 미지정';
 
   const cleanJobId = jobId.replace('.png', '');
   const imageUrl = `${process.env.PUBLIC_URL}/img/${cleanJobId}.png`;
 
-  console.log("jobId:", jobId);
-  console.log("jobName:", jobName);
-  console.log("imageUrl:", imageUrl);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div className="LicenseRecommendPage">
@@ -23,8 +20,20 @@ function LicenseRecommendPage() {
       </div>
 
       <div className="LicenseCard">
-        <img src={imageUrl} alt="자격증 이미지" className="license-image" />
+        <img
+          src={imageUrl}
+          alt="자격증 이미지"
+          className="license-image"
+          onClick={() => setIsModalOpen(true)}
+          style={{ cursor: 'zoom-in' }}
+        />
       </div>
+
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={() => setIsModalOpen(false)}>
+          <img src={imageUrl} alt="확대된 자격증 이미지" className="modal-image" />
+        </div>
+      )}
     </div>
   );
 }
